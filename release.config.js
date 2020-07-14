cd9bba8e23416b5f803ab3bac6f4b002ee801523
mengfei0053 <mengfei0053@live.com>
2020-07-14 11:08:54 +0800

    ci(project): add semantic-release

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  repositoryUrl: "git://github.com/mengfei0053/ynrm.git",
+  branches: ["master", { name: "beta", prerelease: true }],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { revert: true, release: "patch" },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "refactor", release: "patch" },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/git",
+  ],
+};
